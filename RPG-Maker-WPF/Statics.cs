344f2da8b3ee5380f0cd947b21b70f2899d89eec
Tim Stadler <timstadler905@googlemail.com>
2016-02-01 22:15:17 +0100

    Added Statics class

---
+++ RPG-Maker-WPF/Statics.cs
@@ -0,0 +1,18 @@
+﻿namespace RPG_Maker_WPF
+{
+	/// <summary>
+	/// Contains different static objects.
+	/// </summary>
+	public static class Statics
+	{
+		/// <summary>
+		/// The minimum map width.
+		/// </summary>
+		public const int MINMAPWIDTH = 15;
+
+		/// <summary>
+		/// The minimum map height.
+		/// </summary>
+		public const int MINMAPHEIGHT = 20;
+	}
+}
