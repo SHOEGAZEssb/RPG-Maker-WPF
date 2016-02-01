a663f3ce5c173ffc1fca548865354d003e171189
Tim Stadler <timstadler905@googlemail.com>
2016-02-01 16:15:00 +0100

    Integrated Caliburn.Micro MVVM framework

---
+++ RPG-Maker-WPF/AppBootstrapper.cs
@@ -0,0 +1,28 @@
+﻿using Caliburn.Micro;
+
+namespace RPG_Maker_WPF
+{
+	/// <summary>
+	/// Bootstrapper used to connect View and ViewModel on startup.
+	/// </summary>
+	internal class AppBootstrapper : BootstrapperBase
+	{
+		/// <summary>
+		/// Ctor.
+		/// </summary>
+		public AppBootstrapper()
+		{
+			Initialize();
+		}
+
+		/// <summary>
+		/// Displays the root View.
+		/// </summary>
+		/// <param name="sender">Ignored.</param>
+		/// <param name="e">Ignored.</param>
+		protected override void OnStartup(object sender, System.Windows.StartupEventArgs e)
+		{
+			DisplayRootViewFor<MainViewModel>();
+		}
+	}
+}
--- RPG-Maker-WPF/MainView.xaml.cs
+++ RPG-Maker-WPF/MainView.xaml.cs
@@ -18,9 +18,9 @@ namespace RPG_Maker_WPF
 	/// <summary>
 	/// Interaction logic for MainWindow.xaml
 	/// </summary>
-	public partial class MainWindow : Window
+	public partial class MainView : Window
 	{
-		public MainWindow()
+		public MainView()
 		{
 			InitializeComponent();
 		}
