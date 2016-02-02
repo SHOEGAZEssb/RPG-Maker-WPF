093cbb7e0a4dd8262bae1af6849a6751392d0595
Tim Stadler <timstadler905@googlemail.com>
2016-02-02 11:08:14 +0100

    Implemented NewProjectView logic

--- RPG-Maker-WPF/ViewModels/NewProjectViewModel.cs
+++ RPG-Maker-WPF/ViewModels/NewProjectViewModel.cs
@@ -1,25 +1,140 @@
 ﻿using Caliburn.Micro;
-using RPG_Maker_WPF.Models;
+using RPG_Maker_WPF.Views;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace RPG_Maker_WPF.ViewModels
 {
+	/// <summary>
+	/// ViewModel for the <see cref="NewProjectView"/>.
+	/// </summary>
 	class NewProjectViewModel : PropertyChangedBase
 	{
+		#region Member
+
+		/// <summary>
+		/// Indicates if the <see cref="ProjectName"/> has been changed
+		/// manually. If so, changing the <see cref="DirectoryName"/> will have
+		/// no effect on the <see cref="ProjectName"/>.
+		/// </summary>
+		private bool _manuallyChangedProjectName;
+
+		/// <summary>
+		/// Indicates if the <see cref="DirectoryName"/> has been changed
+		/// manually. If so, changing the <see cref="ProjectName"/> will have
+		/// no effect on the <see cref="DirectoryName"/>.
+		/// </summary>
+		private bool _manuallyChangedDirectoryName;
+
+		#endregion Member
+
 		#region Properties
 
-		#endregion
+		/// <summary>
+		/// The name of the new project.
+		/// </summary>
+		public string ProjectName
+		{
+			get { return _projectName; }
+			set
+			{
+				_projectName = value;
+				NotifyOfPropertyChange(() => ProjectName);
+				NotifyOfPropertyChange(() => CanCreate);
+			}
+		}
+		private string _projectName;
+
+		/// <summary>
+		/// The name of the directory the project will
+		/// be saved in.
+		/// </summary>
+		public string DirectoryName
+		{
+			get { return _directoryName; }
+			set
+			{
+				_directoryName = value;
+				NotifyOfPropertyChange(() => DirectoryName);
+				NotifyOfPropertyChange(() => CanCreate);
+			}
+		}
+		private string _directoryName;
+
+		/// <summary>
+		/// The path of the directory the project directory
+		/// will be saved in.
+		/// </summary>
+		public string SaveDirectory
+		{
+			get { return _saveDirectory; }
+			set
+			{
+				_saveDirectory = value;
+				NotifyOfPropertyChange(() => SaveDirectory);
+			}
+		}
+		private string _saveDirectory;
+
+		#region Read-Only Properties
+
+		/// <summary>
+		/// Checks if the given information is valid
+		/// and a project can be created.
+		/// </summary>
+		public bool CanCreate
+		{
+			get { return ProjectName != "" && DirectoryName != ""; }
+		}
+
+		#endregion Read-Only Properties
+
+		#endregion Properties
 
 		/// <summary>
 		/// Constructor.
 		/// </summary>
 		public NewProjectViewModel()
 		{
+			ProjectName = "SampleProject";
+			DirectoryName = "SampleProject";
+			SaveDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\SampleProject";
+		}
 
+		/// <summary>
+		/// Opens a <see cref="FolderBrowserDialog"/> where the user
+		/// can choose where to save his new project.
+		/// </summary>
+		public void SelectSaveDirectory()
+		{
+			FolderBrowserDialog dlg = new FolderBrowserDialog();
+			dlg.SelectedPath = SaveDirectory;
+			if (dlg.ShowDialog() == DialogResult.OK)
+				SaveDirectory = dlg.SelectedPath;
+		}
+
+		/// <summary>
+		/// Creates the project with the given data.
+		/// </summary>
+		/// <param name="view">Reference to the calling <see cref="NewProjectView"/>,
+		/// used to set its DialogResult to true.
+		/// </param>
+		public void Create(NewProjectView view)
+		{
+			view.DialogResult = true;
+			view.Close();
+		}
+
+		/// <summary>
+		/// Cancels the dialog.
+		/// </summary>
+		/// <param name="view">Reference to the calling <see cref="NewProjectView"/>,
+		/// used to set its DialogResult to false.
+		/// </param>
+		public void Cancel(NewProjectView view)
+		{
+			view.DialogResult = false;
+			view.Close();
 		}
 	}
 }
--- RPG-Maker-WPF/Views/NewProjectView.xaml.cs
+++ RPG-Maker-WPF/Views/NewProjectView.xaml.cs
@@ -1,24 +1,15 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
 namespace RPG_Maker_WPF.Views
 {
 	/// <summary>
-	/// Interaction logic for NewProjectView.xaml
+	/// Interaction logic for the <see cref="NewProjectView"/>.
 	/// </summary>
 	public partial class NewProjectView : Window
 	{
+		/// <summary>
+		/// Constructor.
+		/// </summary>
 		public NewProjectView()
 		{
 			InitializeComponent();
