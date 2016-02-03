ee22b95c989ac0d458c6b372517795ae62d6aa05
Tim Stadler <timstadler905@googlemail.com>
2016-02-03 11:14:31 +0100

    Added View and ViewModel for Database, added Actor class

---
+++ RPG-Maker-WPF/Models/Actor.cs
@@ -0,0 +1,50 @@
+﻿namespace RPG_Maker_WPF.Models
+{
+	/// <summary>
+	/// Represents a (theoretically) playable character.
+	/// </summary>
+	class Actor
+	{
+		#region Properties
+
+		/// <summary>
+		/// The name of this actor.
+		/// </summary>
+		public string Name
+		{
+			get { return _name; }
+		  set { _name = value; }
+		}
+		private string _name;
+
+		public string Description
+		{
+			get { return _description; }
+			set { _description = value; }
+		}
+		private string _description;
+
+		/// <summary>
+		/// The initial level of this actor.
+		/// </summary>
+		public int InitialLevel
+		{
+			get { return _initialLevel; }
+			set { _initialLevel = value; }
+		}
+		private int _initialLevel;
+
+		/// <summary>
+		/// The maximum level of this actor.
+		/// </summary>
+		public int MaxLevel
+		{
+			get { return _maxLevel; }
+			set
+			{ _maxLevel = value; }
+		}
+		private int _maxLevel;
+
+		#endregion Properties
+	}
+}
